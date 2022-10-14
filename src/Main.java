1ca03f1337a06fd894b896cff918275e3e2cc696
magnusgg <boostermagnus@gmail.com>
2022-10-14 16:11:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Değişkenleri oluşturalım
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        //Scanner sınıfımızı tanımladık
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıdan değerleri al
+        System.out.print("Matematik Notunuz : ");
+        mat = input.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik = input.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam/6.0;
+        System.out.println("Ortalamanız : " + sonuc);
+
+        boolean gecer = (sonuc > 60.0);
+        String son = (gecer)? "Geçti" : "Kaldı";
+        System.out.println(son);
+
+
+    }
+}
