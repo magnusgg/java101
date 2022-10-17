df2553b4365daba7a7309759647909a71cce2430
magnusgg <boostermagnus@gmail.com>
2022-10-17 15:15:07 +0300

    ucgenalan

---
+++ practice/alanbulma.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class alanbulma {
+    public static void main(String[] args) {
+        double kenar1, kenar2, kenar3, u, alankare;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Birinci Kenar Uzunluğu : ");
+        kenar1 = input.nextDouble();
+
+        System.out.println("İkinci Kenar Uzunluğu : ");
+        kenar2 = input.nextDouble();
+
+        System.out.println("Üçüncü Kenar Uzunluğu : ");
+        kenar3 = input.nextDouble();
+
+        u = (kenar1 + kenar2 + kenar3)/2 ;
+
+
+        alankare = (u * (u - kenar1) * (u - kenar2) * (u - kenar3));
+        System.out.println("Alan : " + Math.sqrt(alankare));
+
+
+
+    }
+}
